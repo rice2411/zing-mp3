eec154b0acc64f3f1e068e00a7ffceb9d677fa92
mai147 <haphuonghoangmai@gmail.com>
2022-10-13 13:15:54 +0700

    Feat/slider

--- src/components/Explore/Recently/data.tsx
+++ src/components/Explore/Recently/data.tsx
@@ -6,37 +6,37 @@ export const data = [
     name: "#zingchart",
   },
   {
-    id: 1,
+    id: 2,
     avatar:
       "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/e/d/2/5/ed251cf560be4747e7737b535c357f07.jpg",
     name: "#zingchart",
   },
   {
-    id: 1,
+    id: 3,
     avatar:
       "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/e/d/2/5/ed251cf560be4747e7737b535c357f07.jpg",
     name: "#zingchart",
   },
   {
-    id: 1,
+    id: 4,
     avatar:
       "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/e/d/2/5/ed251cf560be4747e7737b535c357f07.jpg",
     name: "#zingchart",
   },
   {
-    id: 1,
+    id: 5,
     avatar:
       "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/e/d/2/5/ed251cf560be4747e7737b535c357f07.jpg",
     name: "#zingchart",
   },
   {
-    id: 1,
+    id: 6,
     avatar:
       "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/e/d/2/5/ed251cf560be4747e7737b535c357f07.jpg",
     name: "#zingchart",
   },
   {
-    id: 1,
+    id: 7,
     avatar:
       "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/e/d/2/5/ed251cf560be4747e7737b535c357f07.jpg",
     name: "#zingchart",
