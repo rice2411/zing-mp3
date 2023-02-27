627c8dacc6879b99591550c380282316b0caa4bc
hoangduc1104 <hoangduc91nda@gmail.com>
2023-02-27 20:13:21 +0700

    feat/album_page

--- src/components/Album/index.tsx
+++ src/components/Album/index.tsx
@@ -1,18 +1,54 @@
 import React from 'react';
+import { BiSortAlt2 } from 'react-icons/bi';
 import useTheme from '../../hooks/useTheme';
 import AlbumInfo from '../Shared/AlbumInfo';
+import AlbumItem from '../Shared/AlbumItem';
 
 const Album = () => {
   const { styles }: any = useTheme();
   return (
-    <div className="xl:flex p-3">
-      <div className="w-[300px] h-[1000px] mr-8 xl:relative">
-        <div className="flex w-[300px] xl:fixed top-[120px]">
+    <div className="xl:flex p-3 w-full">
+      <div className="w-[300px] mr-8 xl:relative mt-4 mb-8">
+        <div className="flex w-[300px] xl:fixed ">
           <AlbumInfo />
         </div>
       </div>
-      <div className="w-[600px] bg-blue-400 h-[1000px]">
-        <p>abc</p>
+      <div className="mt-4 flex-1">
+        <div className="text-sm hidden xl:block mb-2.5">
+          <span className={`${styles.body.subTextColor}  `}>Lời tựa </span>
+          <span className={`${styles.body.textColor} `}>
+            Những bản V-Pop 'thuộc nằm lòng' của mọi nhà
+          </span>
+        </div>
+        <div
+          className={`${styles.body.subTextColor} grid grid-cols-12 w-full p-2.5 text-sm`}
+        >
+          <div className="col-span-6 flex">
+            <div className="text-xs border border-[#86828C] my-auto mr-3 rounded w-[14px] h-[14px]">
+              <BiSortAlt2 />
+            </div>
+            <p>Bài hát</p>
+          </div>
+          <div className="col-span-4">
+            <p>Album</p>
+          </div>
+          <div className="text-right col-span-2">
+            <p>Thời gian</p>
+          </div>
+        </div>
+        <div>
+          <AlbumItem active />
+          <AlbumItem />
+          <AlbumItem />
+          <AlbumItem />
+          <AlbumItem />
+          <AlbumItem />
+          <AlbumItem />
+          <AlbumItem />
+          <AlbumItem />
+          <AlbumItem />
+          <AlbumItem />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Shared/AlbumItem/index.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { BiSortAlt2 } from 'react-icons/bi';
+import { BsMusicNoteBeamed, BsPlayFill } from 'react-icons/bs';
+import { FiMoreHorizontal } from 'react-icons/fi';
+import { GiMicrophone } from 'react-icons/gi';
+import useTheme from '../../../hooks/useTheme';
+import ButtonIcon from '../../../shared/small_components/Button';
+
+const AlbumItem = ({ active }: any) => {
+  const { styles }: any = useTheme();
+  return (
+    <>
+      <div
+        className={`group grid grid-cols-12 p-2.5 border-y border-[#273143] rounded hover:bg-[#273143] ${
+          active && 'bg-[#333c4d]'
+        }`}
+      >
+        <div className="col-span-6 flex">
+          <div className={`${styles.body.subTextColor} text-sm relative flex`}>
+            <div className=" my-auto mr-3 rounded w-[14px] h-[14px] ">
+              <BsMusicNoteBeamed
+                className={`${active ? 'hidden' : 'block'} group-hover:hidden`}
+              />
+              <input
+                type="checkbox"
+                className={`my-auto mr-3 rounded bg-transparent checked:bg-transparent focus:shadow-none focus:text-white ${
+                  active ? 'block' : 'hidden'
+                } group-hover:block cursor-pointer`}
+              />
+            </div>
+          </div>
+          <div className="w-[40px] h-[40px] overflow-hidden rounded relative cursor-pointer">
+            <img
+              src="https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_webp/cover/e/7/7/2/e772358978fef8a02eefd34f6a4ca6f3.jpg"
+              alt=""
+            />
+            <div className="text-xl text-white absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 hidden group-hover:block hover:opacity-80">
+              <BsPlayFill />
+            </div>
+          </div>
+
+          <div className="overflow-hidden mr-4 ml-3">
+            <h3
+              className={`${styles.body.textColor} text-sm font-medium truncate`}
+            >
+              Vi mẹ anh bắt chia tay
+            </h3>
+            <a
+              className={`${styles.body.subTextColor} text-xs mt-1 font-normal truncate cursor-pointer hover:decoration-1 hover:underline`}
+            >
+              MONO
+            </a>
+          </div>
+        </div>
+        <div
+          className={` group-hover:col-span-2 2xl:group-hover:col-span-3 my-auto overflow-hidden truncate col-span-4 ${styles.body.subTextColor}`}
+        >
+          <a
+            className={`${styles.body.subTextColor} text-xs font-normal  cursor-pointer hover:decoration-1 hover:underline hover:text-[#c662ef]`}
+          >
+            Vì mẹ anh bắt chia tay (Single)
+          </a>
+        </div>
+        <div
+          className={` group-hover:col-span-4 2xl:group-hover:col-span-3 my-auto col-span-2`}
+        >
+          <h4
+            className={`${styles.body.subTextColor} text-xs font-normal truncate text-right mr-2 group-hover:hidden`}
+          >
+            04:12
+          </h4>
+          <div className={`hidden group-hover:block text-right`}>
+            <ButtonIcon
+              className={`hover:bg-[hsla(0,0%,100%,0.1)] ${styles.body.textColor}  h-10 w-10`}
+            >
+              <GiMicrophone />
+            </ButtonIcon>
+            <ButtonIcon
+              className={`hover:bg-[hsla(0,0%,100%,0.1)] ${styles.body.textColor}  h-10 w-10`}
+            >
+              <AiOutlineHeart />
+            </ButtonIcon>
+
+            <ButtonIcon
+              className={`hover:bg-[hsla(0,0%,100%,0.1)] ${styles.body.textColor} h-10 w-10`}
+            >
+              <FiMoreHorizontal />
+            </ButtonIcon>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AlbumItem;
