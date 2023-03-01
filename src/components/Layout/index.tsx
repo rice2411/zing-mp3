4c3378090ec69db7ab20157425cc6251d32f490b
mai147 <haphuonghoangmai@gmail.com>
2023-03-01 17:25:50 +0700

    Fix scroll

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,24 +7,24 @@ import Navbar from "./Navbar";
 import Scrollbar from "../../shared/small_components/Scrollbar";
 
 export default function Layout({ route, ...props }: any) {
-  const { handleChangeTheme }: any = useTheme();
-  const { styles }: any = useTheme();
+    const { handleChangeTheme }: any = useTheme();
+    const { styles }: any = useTheme();
 
-  return (
-    <div className={`${styles.body.backgroundColor}`}>
-      <div className="flex h-screen">
-        <SiderBar />
-        <div className="flex flex-col w-full h-[calc(100vh_-_90px)] ">
-          <div className="mx-10">
-            <Navbar />
-          </div>
-          <Scrollbar isHover={true} className="">
-            <div className="mx-10 ">{props.children}</div>
-          </Scrollbar>
+    return (
+        <div className={`${styles.body.backgroundColor}`}>
+            <div className="flex h-screen">
+                <SiderBar />
+                <div className="flex flex-col w-full h-[calc(100vh_-_90px)] ">
+                    <div className="mx-10">
+                        <Navbar />
+                    </div>
+                    <Scrollbar isHover={true} className="">
+                        <div className="mx-10 ">{props.children}</div>
+                    </Scrollbar>
+                </div>
+            </div>
+
+            <Modal />
         </div>
-      </div>
-
-      <Modal />
-    </div>
-  );
+    );
 }
