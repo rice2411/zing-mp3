425a89184b217f500706e612ffd7b622eac35eeb
rice <pentappingminh@gmail.com>
2023-04-24 14:24:21 +0700

    test

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,6 +8,7 @@ import Scrollbar from "../../shared/small_components/Scrollbar";
 import useAudio from "../../hooks/useAudio";
 import ModalVip from "../Shared/ModalVip";
 import { useLocation } from "react-router-dom";
+import "./styles.css";
 
 export default function Layout({ route, ...props }: any) {
   const location = useLocation();
@@ -16,31 +17,34 @@ export default function Layout({ route, ...props }: any) {
 
   return (
     <div className={`${styles.body.backgroundColor}`}>
-      {isShowLyrics ? (
-        <div className="bg-red-500 w-full h-full"></div>
-      ) : (
-        <div className="flex h-screen">
-          <SiderBar />
-          <div
-            className={`flex flex-col w-screen  ${
-              audio ? "h-[calc(100vh_-_90px)]" : ""
-            }  pl-[calc(100vw - 100%)]`}
-          >
-            <div className="">
-              <Navbar />
-            </div>
-            <Scrollbar isHover={true} className="">
-              <div
-                className={`${
-                  location.pathname.includes("hub/detail") ? "" : "mx-10"
-                }`}
-              >
-                {props.children}
-              </div>
-            </Scrollbar>
-          </div>
+      <div className="flex h-screen box relative  ">
+        <div className={`hid-box `}>
+          <h1>CSS3 slide up</h1>
+          <p>
+            This is a quick demo of slide-up effect using CSS animation when
+            hover the box. No JS required!
+          </p>
         </div>
-      )}
+        <SiderBar />
+        <div
+          className={`flex flex-col w-screen  ${
+            audio ? "h-[calc(100vh_-_90px)]" : ""
+          }  pl-[calc(100vw - 100%)]`}
+        >
+          <div className="">
+            <Navbar />
+          </div>
+          <Scrollbar isHover={true} className="">
+            <div
+              className={`${
+                location.pathname.includes("hub/detail") ? "" : "mx-10"
+              }`}
+            >
+              {props.children}
+            </div>
+          </Scrollbar>
+        </div>
+      </div>
 
       <Modal />
       <ModalVip
