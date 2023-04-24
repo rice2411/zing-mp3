bf6dbc913bc2c2d121d42a224b17689e72993aec
rice <pentappingminh@gmail.com>
2023-04-24 11:37:09 +0700

    test ignore

--- src/shared/small_components/Button/Icon/index.tsx
+++ src/shared/small_components/Button/Icon/index.tsx
@@ -5,7 +5,7 @@ const ButtonIcon = ({ onClick, className, refButton, ...props }: any) => {
     <button
       ref={refButton}
       onClick={onClick}
-      className={`inline-flex items-center justify-center mr-2  transition-colors duration-150  ${className}`}
+      className={`inline-flex items-center justify-center mr-2 transition-colors duration-150  ${className}`}
     >
       {props.children}
     </button>
